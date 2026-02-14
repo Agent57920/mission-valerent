6388a01ec94a8f8aeb3e5544f0e720f32bc7beed
Agent57920 <arnavabrol8@gmail.com>
2026-02-14 15:15:17 +0530

    Add files via upload

---
+++ level8.js
@@ -0,0 +1,124 @@
+const o1 = document.getElementById('overlay1');
+const o2 = document.getElementById('overlay2');
+const o3 = document.getElementById('overlay3');
+const o4 = document.getElementById('overlay4');
+const o5 = document.getElementById('overlay5');
+const o6 = document.getElementById('overlay6');
+const o7 = document.getElementById('overlay7');
+const o8 = document.getElementById('overlay8');
+const o9 = document.getElementById('overlay9');
+const o10 = document.getElementById('overlay10');
+const o11 = document.getElementById('overlay11');
+const o12 = document.getElementById('overlay12');
+const o13 = document.getElementById('overlay13');
+const o14 = document.getElementById('overlay14');
+const o15 = document.getElementById('overlay15');
+const o16 = document.getElementById('overlay16');
+const o17 = document.getElementById('overlay17');
+const o18 = document.getElementById('overlay18');
+const o19 = document.getElementById('overlay19');
+const sub = document.getElementById('sub');
+const sub1 = document.getElementById('sub1');
+const sub2 = document.getElementById('sub2');
+const sub3 = document.getElementById('sub3');
+const sub4 = document.getElementById('sub4');
+const sub5 = document.getElementById('sub5');
+const sub6 = document.getElementById('sub6');
+const sub7 = document.getElementById('sub7');
+const sub8 = document.getElementById('sub8');
+const sub9 = document.getElementById('sub9');
+const sub10 = document.getElementById('sub10');
+const sub11 = document.getElementById('sub11');
+const sub12 = document.getElementById('sub12');
+const sub13 = document.getElementById('sub13');
+const sub14 = document.getElementById('sub14');
+const sub15 = document.getElementById('sub15');
+const sub16 = document.getElementById('sub16');
+const sub17 = document.getElementById('sub17');
+const sub18 = document.getElementById('sub18');
+const sub19 = document.getElementById('sub19');
+const vdo = document.getElementById('vdo');
+
+sub.addEventListener('click', () => {
+    o1.style.display = 'block'
+})
+sub1.addEventListener('click', () => {
+    o2.style.display = 'block'
+    o1.style.display = 'none'
+})
+sub2.addEventListener('click', () => {
+    o3.style.display = 'block'
+    o2.style.display = 'none'
+})
+sub3.addEventListener('click', () => {
+    o4.style.display = 'block'
+    o3.style.display = 'none'
+})
+sub4.addEventListener('click', () => {
+    o5.style.display = 'block'
+    o4.style.display = 'none'
+})
+sub5.addEventListener('click', () => {
+    o6.style.display = 'block'
+    o5.style.display = 'none'
+})
+sub6.addEventListener('click', () => {
+    o7.style.display = 'block'
+    o6.style.display = 'none'
+})
+sub7.addEventListener('click', () => {
+    o8.style.display = 'block'
+    o7.style.display = 'none'
+})
+sub8.addEventListener('click', () => {
+    o9.style.display = 'block'
+    o8.style.display = 'none'
+})
+sub9.addEventListener('click', () => {
+    o10.style.display = 'block'
+    o9.style.display = 'none'
+})
+sub10.addEventListener('click', () => {
+    o11.style.display = 'block'
+    o10.style.display = 'none'
+})
+sub11.addEventListener('click', () => {
+    o12.style.display = 'block'
+    o11.style.display = 'none'
+})
+sub12.addEventListener('click', () => {
+    o13.style.display = 'block'
+    o12.style.display = 'none'
+})
+sub13.addEventListener('click', () => {
+    o14.style.display = 'block'
+    o13.style.display = 'none'
+})
+sub14.addEventListener('click', () => {
+    o15.style.display = 'block'
+    o14.style.display = 'none'
+})
+sub15.addEventListener('click', () => {
+    o16.style.display = 'block'
+    o15.style.display = 'none'
+})
+sub16.addEventListener('click', () => {
+    o17.style.display = 'block'
+    o16.style.display = 'none'
+})
+sub17.addEventListener('click', () => {
+    o18.style.display = 'block'
+    o17.style.display = 'none'
+})
+sub18.addEventListener('click', () => {
+    o19.style.display = 'block'
+    o18.style.display = 'none'
+})
+sub19.addEventListener('click', () => {
+    o19.style.display = 'none'
+    sub.style.display = 'none'
+    vdo.style.display = 'block'
+    vdo.play()
+
+    
+})
